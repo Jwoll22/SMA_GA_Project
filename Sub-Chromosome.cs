e7762cd0a3af84bef62405af910500ffa08e677d
John3B <johnburgh3219@hotmail.com>
2015-11-24 08:21:37 -0500

    Update Sub-Chromosome.cs

--- Sub-Chromosome.cs
+++ Sub-Chromosome.cs
@@ -12,6 +12,9 @@ namespace WindowsFormsApplication1
         //  consecutive ones and zeros 
         static private int subChromLength = 0;
 
+        // add public int[] Genes
+        https://github.com/John3B/SMA_GA_Project
+        // properties
         public static int Length
         {
             get
